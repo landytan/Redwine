d6dc71b19f79a3e316ebeb4dcf33abc345a0f771
landytan <33996664+landytan@users.noreply.github.com>
2020-04-27 17:48:39 +0800

    Add files via upload

---
+++ Redwine.py
@@ -0,0 +1,144 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+
+
+
+
+#了解数据
+wine = pd.read_csv('../input/winequality-red.csv')
+
+wine.head()
+wine.info()
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'fixed acidity', data = wine)
+
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'volatile acidity', data = wine)
+
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'citric acid', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'residual sugar', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'chlorides', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'free sulfur dioxide', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'total sulfur dioxide', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'sulphates', data = wine)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'alcohol', data = wine)
+
+#二元化
+bins = (2, 6.5, 8)
+group_names = ['bad', 'good']
+wine['quality'] = pd.cut(wine['quality'], bins = bins, labels = group_names)
+
+#设定标签
+label_quality = LabelEncoder()
+
+
+wine['quality'] = label_quality.fit_transform(wine['quality'])
+
+
+wine['quality'].value_counts()
+
+
+sns.countplot(wine['quality'])
+
+#确定训练集和测试集
+X = wine.drop('quality', axis = 1)
+y = wine['quality']
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+#数据正态化
+sc = StandardScaler()
+
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+rfc = RandomForestClassifier(n_estimators=200)
+rfc.fit(X_train, y_train)
+pred_rfc = rfc.predict(X_test)
+
+
+print(classification_report(y_test, pred_rfc))
+
+
+print(confusion_matrix(y_test, pred_rfc))
+
+
+#尝试Stochastic Gradient Decent Classifier
+
+
+sgd = SGDClassifier(penalty=None)
+sgd.fit(X_train, y_train)
+pred_sgd = sgd.predict(X_test)
+
+
+print(classification_report(y_test, pred_sgd))
+
+
+print(confusion_matrix(y_test, pred_sgd))
+
+
+
+svc = SVC()
+svc.fit(X_train, y_train)
+pred_svc = svc.predict(X_test)
+
+
+print(classification_report(y_test, pred_svc))
+
+
+#提高准确率
+
+#Grid Search CV
+
+#调参
+param = {
+    'C': [0.1,0.8,0.9,1,1.1,1.2,1.3,1.4],
+    'kernel':['linear', 'rbf'],
+    'gamma' :[0.1,0.8,0.9,1,1.1,1.2,1.3,1.4]
+}
+grid_svc = GridSearchCV(svc, param_grid=param, scoring='accuracy', cv=10)
+
+
+grid_svc.fit(X_train, y_train)
+
+
+grid_svc.best_params_
+
+
+svc2 = SVC(C = 1.2, gamma =  0.9, kernel= 'rbf')
+svc2.fit(X_train, y_train)
+pred_svc2 = svc2.predict(X_test)
+print(classification_report(y_test, pred_svc2))
+
+
+rfc_eval = cross_val_score(estimator = rfc, X = X_train, y = y_train, cv = 10)
+rfc_eval.mean()
+
